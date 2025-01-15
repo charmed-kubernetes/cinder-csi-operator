3498c701d25c4197f90a45565fd981475b4077ef
Adam Dyess <adam.dyess@canonical.com>
2025-01-15 10:39:18 -0600

    Map cinder-csi to juju terraform syntax (#10)
    * Map cinder-csi to juju terraform syntax
    * Can't use constraints on subordinates

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+resource "juju_application" "cinder_csi" {
+  name  = var.app_name
+  model = var.model
+
+  charm {
+    name     = "cinder-csi"
+    channel  = var.channel
+    revision = var.revision
+    base     = var.base
+  }
+
+  config      = var.config
+}
